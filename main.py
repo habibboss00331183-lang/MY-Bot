b142c01720e1e2a556ded3e3192e658b69908123
habibboss00331183-lang <habibboss00331183@gmail.com>
2026-08-18 09:17:34 +0600

    Update main.py

--- main.py
+++ main.py
@@ -258,6 +258,7 @@ def main():
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button_handler))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, message_handler))
+  
     app.run_polling()
 
 if __name__ == "__main__":
